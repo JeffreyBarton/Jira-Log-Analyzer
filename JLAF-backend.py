c28d02f3a08495043665703ab452db86d46d7ee7
LehTachos <jeffbarton17@gmail.com>
2016-08-03 16:12:01 -0600

    Added File Viewer, and File Selection

--- JLAF-backend.py
+++ JLAF-backend.py
@@ -1,4 +1,5 @@
-import sys
+import sys, pathlib
+from os import walk
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from JLAF import Ui_myfirstgui
@@ -10,24 +11,47 @@ class MyFirstGuiProgram(Ui_myfirstgui):
 		self.setupUi(dialog)
 	
 		self.fname = ""
+		self.activeFile = ""
 		self.directory = ""
 
 		self.actionOpen_File.triggered.connect(self.showOpenFile)
 		self.commandLinkButton.clicked.connect(self.analyzeLogs)
+		self.listWidget_2.itemClicked.connect(self.changeActiveFile)
+		
 
 	def showOpenFile(self):
 
 		self.fname = QFileDialog.getOpenFileName(dialog, 'Open file')
-		self.label_3.setText(str(self.fname[0]))
-		
+		self.activeFile = str(self.fname[0])
+		self.label_3.setText(self.activeFile)
+		self.listWidget_2.clear()
+		f = []
+		for (dirpath, dirnames, filenames) in walk(str(self.getDir(self.activeFile))):
+			f.extend(filenames)
+			self.listWidget_2.addItems(filenames)
+			break
+
+	def changeActiveFile(self):
+		#print(str(self.directory) + "\\" + str(self.listWidget_2.currentItem().text()))
+		self.activeFile = str(str(self.directory) + "\\" + str(self.listWidget_2.currentItem().text()))
+		self.label_3.setText(self.activeFile)
+
 	def analyzeLogs(self):
 		self.listWidget.clear()
-		f = open(self.fname[0],'r')
-		word_to_check = self.getlist()
-		for line in f:
-			if any(word in line for word in word_to_check):
-				self.listWidget.addItem(line)
+		try:
+			f = open(self.activeFile,'r')
+			word_to_check = self.getlist()
+			for line in f:
+				if any(word in line for word in word_to_check):
+					self.listWidget.addItem(line)
+		except:
+			 self.label_3.setText(str("No file selected"))
 
+	def getDir(self,path_name):
+		existGDBPath = pathlib.Path(path_name)
+		wkspFldr = existGDBPath.parent
+		self.directory = wkspFldr
+		return wkspFldr
 
 	def getlist(self):
 		words_to_check = []
